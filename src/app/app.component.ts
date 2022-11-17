a820bebc75e0d752fe1a226857793845a6ec316f
Kurtyka03 <kurtyka03@gmail.com>
2022-11-17 15:00:25 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-base';
+  daneKursanta: string = '';
+  numerKursu: number = 0;
+  array = ['Programowanie w C#', 'Angular dla poczatkujacych', 'Kurs Django']
+  Add() {
+    console.log(this.daneKursanta)
+    if(this.numerKursu > 0 && this.numerKursu < 4){
+      console.log(this.array[this.numerKursu - 1])
+    }
+    else{
+      console.log('Niewlasciwy numer')
+    }
+
+  }
 }
